a355ebb63048f644737573612725f749e54c10ef
Borys Smejda <borys.smejda@gmail.com>
2025-05-11 14:53:41 +0200

    Fixed tests according to suggestions from chatgpt.

--- tests/numeric/test_lsqe.cpp
+++ tests/numeric/test_lsqe.cpp
@@ -18,8 +18,7 @@ TEST_CASE("Least square estimation method for incorrect input values", "[lsqe]")
 
     SECTION("Empty x values")
     {
-        REQUIRE(x.empty());
-        REQUIRE_FALSE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+        REQUIRE_FALSE(dsa_lsqe(nullptr, y.data(), x.size(), &a, &b));
     }
 
     x.push_back(-4.0);
@@ -27,8 +26,7 @@ TEST_CASE("Least square estimation method for incorrect input values", "[lsqe]")
     SECTION("Empty y values")
     {
         REQUIRE_FALSE(x.empty());
-        REQUIRE(y.empty());
-        REQUIRE_FALSE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+        REQUIRE_FALSE(dsa_lsqe(x.data(), nullptr, x.size(), &a, &b));
     }
 
     y.push_back(0.0);
@@ -77,4 +75,66 @@ TEST_CASE("Least square estimation method for correct input", "[lsqe]")
 
     REQUIRE_THAT(a, Catch::Matchers::WithinAbs(0.5519, 0.0001));
     REQUIRE_THAT(b, Catch::Matchers::WithinAbs(-0.0249, 0.0001));
+}
+
+TEST_CASE("Least square estimation method on unsorted x values", "[lsqe]")
+{
+    constexpr std::array<double, 9> x{2.0, 3.5, 1.0, -3.0, -2.0, -1.5, -0.5, 4.0, -4.0};
+    constexpr std::array<double, 9> y{1.5, 1.0, 0.0, -1.0, -2.0, -0.5, 1.0, 2.5, -3.0};
+
+    double a{0.0};
+    double b{0.0};
+
+    REQUIRE((x.size() == y.size() && x.size() > 2));
+    REQUIRE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+
+    REQUIRE_THAT(a, Catch::Matchers::WithinAbs(0.5519, 0.0001));
+    REQUIRE_THAT(b, Catch::Matchers::WithinAbs(-0.0249, 0.0001));
+}
+
+TEST_CASE("Least square estimation with perfectly linear values", "[lsqe]")
+{
+    SECTION("Positive Slope")
+    {
+        constexpr std::array<double, 3> x{0.0, 1.0, 2.0};
+        constexpr std::array<double, 3> y{1.0, 3.0, 5.0};
+
+        double a{0.0};
+        double b{0.0};
+
+        REQUIRE((x.size() == y.size() && x.size() > 2));
+        REQUIRE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+
+        REQUIRE_THAT(a, Catch::Matchers::WithinAbs(2.0, 0.0001));
+        REQUIRE_THAT(b, Catch::Matchers::WithinAbs(1.0, 0.0001));
+    }
+
+    SECTION("Negative Slope")
+    {
+        constexpr std::array<double, 3> x{1.0, 2.0, 3.0};
+        constexpr std::array<double, 3> y{6.0, 4.0, 2.0};
+
+        double a{0.0};
+        double b{0.0};
+
+        REQUIRE((x.size() == y.size() && x.size() > 2));
+        REQUIRE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+
+        REQUIRE_THAT(a, Catch::Matchers::WithinAbs(-2.0, 0.0001));
+        REQUIRE_THAT(b, Catch::Matchers::WithinAbs(8.0, 0.0001));
+    }
+}
+
+TEST_CASE("Least square estimation with large input", "[lsqe]")
+{
+    std::vector<double> x(10000), y(10000);
+    for (size_t i = 0; i < x.size(); ++i) {
+        x[i] = static_cast<double>(i);
+        y[i] = 3.0 * x[i] + 1.0;
+    }
+
+    double a{0.0}, b{0.0};
+    REQUIRE(dsa_lsqe(x.data(), y.data(), x.size(), &a, &b));
+    REQUIRE_THAT(a, Catch::Matchers::WithinAbs(3.0, 0.0001));
+    REQUIRE_THAT(b, Catch::Matchers::WithinAbs(1.0, 0.0001));
 }
