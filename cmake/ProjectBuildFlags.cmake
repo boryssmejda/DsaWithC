22b1d911565ef936f76e551bcacc53fc14400515
Borys Smejda <33206513+boryssmejda@users.noreply.github.com>
2025-05-11 22:13:55 +0200

    #2 Compiled library with tests on Windows with MSVC (#23)
    - Dropped C23 standard for C17 as MSVC does not support C23 standard.
    - Adjusted code so that it compiles in C17 mode.

--- cmake/ProjectBuildFlags.cmake
+++ cmake/ProjectBuildFlags.cmake
@@ -3,20 +3,30 @@ add_library(dsa::build_flags ALIAS build_flags)
 
 target_compile_features(build_flags
     INTERFACE
-        c_std_23)
+        c_std_17)
 
-target_compile_options(build_flags
-    INTERFACE
-        -Werror
-        -Wall
-        -Wextra
-        -Wshadow
-        -Wcast-align
-        -Wunused
-        -Wpedantic
-        -Wconversion
-        -Wsign-conversion
-        -Wnull-dereference
-        -Wdouble-promotion
-        -Wformat=2
-        -Wimplicit-fallthrough)
+if (CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(build_flags
+        INTERFACE
+            /W4             # Enable level 4 warnings
+            /WX             # Treat warnings as errors
+            /permissive-    # Enforce standard conformance
+    )
+else()
+    target_compile_options(build_flags
+        INTERFACE
+            -Werror
+            -Wall
+            -Wextra
+            -Wshadow
+            -Wcast-align
+            -Wunused
+            -Wpedantic
+            -Wconversion
+            -Wsign-conversion
+            -Wnull-dereference
+            -Wdouble-promotion
+            -Wformat=2
+            -Wimplicit-fallthrough
+    )
+endif()
