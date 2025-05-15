6bcf0594818e5d5f6fe1dd857d4518e5be0e55ab
Borys Smejda <borys.smejda@gmail.com>
2025-05-15 22:29:56 +0200

    Fixed doxygen comments.

--- include/dsa/numeric/root.h
+++ include/dsa/numeric/root.h
@@ -4,32 +4,36 @@
 #include <stddef.h>
 
 /**
- * @brief  Computes the root of f to which Newton’s method
- * converges given an initial iteration point.
+ * @brief Finds a root of the function @p f using Newton's method.
  *
- * Computes the root of f to which Newton’s method converges
- * given an initial iteration point. This point is specified in x[0].
- * The derivative of f is specified in g. The argument n is the maximum
- * number of iterations to perform. The argument delta is the difference
- * between successive approximations at which to stop iterating.
- * Upon return, successive values of x calculated during the
- * iteration process are returned in the x array.
- * Upon return, n contains the number of values in array x.
- * It is the responsibility of the caller to manage the storage associated with x.
+ * Uses Newton's method to approximate a root of the function @p f,
+ * starting from the initial guess stored in @p x[0]. The derivative
+ * of @p f must be provided in @p g.
  *
- * @param[in] f
- * @param[in] g Derivative of @p f
- * @param[in,out] x Solutions at steps
- * @param[in,out] n Maximum numbers of iterations to perform.
- * @param[in] delta Difference between successive approximations at which to stop iterating.
+ * The caller must allocate an array @p x of at least @p *n elements.
+ * The function updates this array with successive approximations.
+ * Iteration stops either when the difference between successive approximations
+ * is less than @p delta, or when @p *n steps have been performed.
  *
- * @return true if a root is found, false otherwise.
+ * On successful completion, @p x will contain the steps of the iteration
+ * and @p *n will be updated to reflect the number of steps taken.
  *
- * @complexity Time complexity: O(n), where n is the maximum number
- * of iterations the caller wishes to perform.
+ * @param[in] f Function whose root is to be found.
+ * @param[in] g Derivative of @p f.
+ * @param[in,out] x Array where x[0] is the initial guess; updated with iteration values.
+ * @param[in,out] n Pointer to the number of iterations:
+ *                  - On input: max number of iterations allowed.
+ *                  - On output: number of iterations actually performed.
+ * @param[in] delta Threshold to stop when |x[i+1] - x[i]| < delta.
+ *
+ * @return true if a root was found within the given tolerance and iterations, false otherwise.
+ *
+ * @note The caller must ensure that the array @p x has at least @p *n elements allocated.
+ *
+ * @complexity O(n), where n is the maximum number of iterations.
  */
 
-bool root(
+bool root_newton(
     double (*f)(double x),
     double (*g)(double x),
     double *x,
