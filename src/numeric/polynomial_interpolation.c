2ccbc3b25da106724dc35ffe64d69e7b8f03f00b
Borys Smejda <borys.smejda@gmail.com>
2025-05-26 20:03:34 +0200

    Created a polynomial interpolation based on recursive approach.

--- src/numeric/polynomial_interpolation.c
+++ src/numeric/polynomial_interpolation.c
@@ -1,13 +1,50 @@
 #include "dsa/numeric/polynomial_interpolation.h"
 
+static double calculate_divided_difference(
+    const double *x,
+    const double *fx,
+    const size_t start,
+    const size_t end)
+{
+    if (start == end)
+    {
+        return fx[start];
+    }
+
+    const double first = calculate_divided_difference(x, fx, start + 1, end);
+    const double second = calculate_divided_difference(x, fx, start, end - 1);
+
+    const double numerator = first - second;
+    const double denominator = x[end] - x[start];
+
+    return numerator / denominator;
+}
+
 bool dsa_interpolate(
     const double *x,
     const double *fx,
     const size_t n,
-    double *z,
+    const double *z,
     double *pz,
     const size_t m)
 {
-    return false;
-}
+    if (!x || !fx || n < 2 || !z || !pz || m == 0)
+    {
+        return false;
+    }
 
+    for (int i = 0; i < m; i++)
+    {
+        pz[i] = calculate_divided_difference(x, fx, 0, 0);
+        double previous = 1;
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            const double divided_difference = calculate_divided_difference(x, fx, 0, j + 1);
+            previous *= (z[i] - x[j]);
+            pz[i] += (divided_difference * previous);
+        }
+    }
+
+    return true;
+}
