efc037b1962514b2ca3acfc19fc8c11e14bbbc18
Borys Smejda <borys.smejda@gmail.com>
2025-06-12 19:41:11 +0200

    Added empty files.

---
+++ include/dsa/utility/reverse.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stddef.h>
+
+void dsa_reverse(void* arr, const size_t n, const size_t size);
---
+++ src/utility/reverse.c
@@ -0,0 +1,12 @@
+#include "dsa/utility/reverse.h"
+
+void dsa_reverse(void* arr, const size_t n, const size_t size)
+{
+    if (!arr || n <= 1 || size == 0)
+    {
+        return;
+    }
+
+    
+
+}
