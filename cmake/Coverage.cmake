2aa562be7d0de08fce5d142b7bd62f4a73326a78
Borys Smejda <borys.smejda@gmail.com>
2025-06-16 23:47:01 +0200

    Finished working on coverage report.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -12,15 +12,21 @@ elseif(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
     message(FATAL_ERROR "Code coverage requires Debug build type")
 endif()
 
+if (NOT CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    message(FATAL_ERROR "Code coverage is only supported with GCC. Try setting -DCMAKE_C_COMPILER=gcc")
+endif()
+
 target_compile_options(build_flags INTERFACE --coverage)
 target_link_options(build_flags INTERFACE --coverage)
 
-find_program(GCOVR_PATH gcovr REQUIRED)
+find_program(GCOVR_PATH NAMES gcovr REQUIRED)
+
+set(COVERAGE_OUTPUT_DIR ${CMAKE_BINARY_DIR}/coverage-report/)
 
 add_custom_target(coverage
     COMMAND ${CMAKE_COMMAND} --build . --target all
     COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
-    COMMAND ${CMAKE_COMMAND} -E make_directory coverage-report
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_OUTPUT_DIR}
     COMMAND ${GCOVR_PATH}
         --root ${CMAKE_SOURCE_DIR}
         --object-directory ${CMAKE_BINARY_DIR}
@@ -28,7 +34,7 @@ add_custom_target(coverage
         --html-details
         --html-title "DSA Code Coverage Report"
         --html-self-contained
-        --output coverage-report/coverage.html
+        --output ${COVERAGE_OUTPUT_DIR}/coverage.html
         --delete
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     COMMENT "Generating code coverage report with gcovr"
