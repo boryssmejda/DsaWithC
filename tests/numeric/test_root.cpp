c1e8b551c039c297cb0fca6d74e1302fd36c71a0
Borys Smejda <borys.smejda@gmail.com>
2025-07-23 00:58:30 +0200

    Added type to template parameter for PI.

--- tests/numeric/test_root.cpp
+++ tests/numeric/test_root.cpp
@@ -49,7 +49,7 @@ TEST_CASE("Non-polynomial function: sin(x)", "[root_newton]")
 
     size_t n = x.size();
     REQUIRE(dsa_find_root_newton(f, g, x.data(), &n, delta) == DSA_ROOT_SUCCESS);
-    REQUIRE_THAT(x[n - 1], Catch::Matchers::WithinAbs(std::numbers::pi_v, delta));
+    REQUIRE_THAT(x[n - 1], Catch::Matchers::WithinAbs(std::numbers::pi_v<double>, delta));
 }
 
 TEST_CASE("Maximum number of iterations exceeded", "[root_newton]")
